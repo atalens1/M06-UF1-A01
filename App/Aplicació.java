37dd60817d52429873383a92916c28058164361f
A.T. <atalens1@xtec.cat>
2024-10-21 01:10:00 +0200

    Canvis en classes i README.md

--- App/Aplicació.java
+++ App/Aplicació.java
@@ -1,21 +1,11 @@
 package App;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.EOFException;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 import Model.Article;
-import Model.Encarrec;
 
 public class Aplicació {
 
@@ -34,12 +24,13 @@ public class Aplicació {
 
     public static void MainMenu() {
 
-        System.out.println("Quina de les següents opcions vols fer?");
+        System.out.println("Opcions disponibles");
         System.out.println("");
         System.out.println("1. Afegir nous encàrrecs");
         System.out.println("2. Mostrar per pantalla els encàrrecs");
         System.out.println("3. Sortir");
         System.out.println("");
+        System.out.print("Quina opció tries?: ");
     }
 
     public static void DemanarOpcio() {
@@ -130,7 +121,9 @@ public class Aplicació {
 
         String extensio = "";
 
-        String fileName = "C:\\Users\\accesadades\\" + "encarrecs_client_" + nomCli + "_"+ System.currentTimeMillis() + extensio;
+        String folder = "C:\\Users\\accesadades\\";
+
+        String fileName = folder + "encarrecs_client_" + nomCli + "_"+ System.currentTimeMillis() + extensio;
 
         System.out.println("En quin format vols escriure el fitxer?: ");
         System.out.println("1. text albarà");
@@ -139,11 +132,13 @@ public class Aplicació {
 
         String tipusFich = reader.readLine();
 
+        UtilWriteFitxer uw1 = new UtilWriteFitxer();
+
         switch (tipusFich) {
             case "1":
                 extensio = ".txt";
                 fileName = fileName.concat(extensio);
-                TextMultiLinea(nomCli,telCli,dataEncarrec,articles,fileName);
+                uw1.TextMultiLinea(nomCli, telCli, dataEncarrec, articles, fileName);
                 break;
             
             case "2": 
@@ -151,13 +146,13 @@ public class Aplicació {
                 // csvLineaObjEn(encarrec, fileName);
                 extensio = ".csv";
                 fileName = fileName.concat(extensio);
-                csvLinea(nomCli,telCli,dataEncarrec,articles,fileName);
+                uw1.csvLinea(nomCli, telCli, dataEncarrec, articles, fileName);
                 break;
 
             case "3":
                 extensio = ".dat";
                 fileName = fileName.concat(extensio);
-                binari(nomCli,telCli,dataEncarrec,articles,fileName);
+                uw1.binari(nomCli,telCli,dataEncarrec,articles,fileName);
                 break;
         
             default:
@@ -181,189 +176,12 @@ public class Aplicació {
 
         String fileName = reader.readLine();
 
+        UtilReadFitxer ur1 = new UtilReadFitxer();
+
         if (opcio.equals("1")) {
-            FormatCSV(folder, fileName);
+            ur1.FormatCSV(folder, fileName);
         } else if (opcio.equals("2")) {
-            FormatBinari(folder, fileName);
-        }
-
-    }
-
-    public static void FormatCSV(String folder, String fileName) {
-        String filInputName = folder + fileName + ".csv";
-
-        try (BufferedReader bwfilInp = new BufferedReader(new FileReader(filInputName))) {
-
-            String linea = "";
-            String[] contingut;
-
-            while ((linea = bwfilInp.readLine()) != null){
-
-                contingut = linea.split(";", 0);
-                String nomCli = contingut[0];
-                String telCli = contingut[1];
-                String dataEncarrec = contingut[2];
-
-                ArrayList<Article> articles = new ArrayList<>();
-
-                int j = 3;
-
-                while (j < contingut.length) {
-                    Article a1 = new Article();
-                    a1.setNomArticle(contingut[j]);
-                    j++;
-                    a1.setnombreUnitats(Float.parseFloat(contingut[j]));
-                    j++;
-                    a1.settipusUnitat(contingut[j]);
-                    articles.add(a1);
-                    j++;
-                }
-
-                FormatEncarrec(nomCli, telCli, dataEncarrec,articles);
-            } 
-
-        } catch (FileNotFoundException fn) {
-                System.out.println ("No es troba el fitxer");         
-        } catch (IOException io) {
-        System.out.println ("Error d'E/S"); 
-        }
-    }
-
-    public static void FormatBinari(String folder, String fileName) {
-
-        String filInputName = folder + fileName + ".dat";
-        ArrayList<Article> articles = new ArrayList<>();
-
-        try (DataInputStream diStr1 = new DataInputStream(new FileInputStream(filInputName))) {
-
-            String nomCli = diStr1.readUTF();
-            String telCli = diStr1.readUTF();
-            String dataEncarrec = diStr1.readUTF();
-
-            try{
-
-                while (diStr1.available()>0) {
-                    String nomArticle = diStr1.readUTF();
-                    float quantitat = diStr1.readFloat();
-                    String unitat = diStr1.readUTF();
-                    Article art = new Article(nomArticle,quantitat,unitat);
-                    articles.add(art);   
-                }
-
-                FormatEncarrec(nomCli, telCli, dataEncarrec,articles);
-
-            } catch (EOFException e) {
-                System.out.println("Final de fitxer");
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public static void FormatEncarrec(String nomCli, String telCli, String dataEncarrec, ArrayList<Article> articles) {
-        System.out.printf(String.format("%n"));
-        System.out.println("DETALL DE L'ENCARREC");
-        System.out.println("====================================================");
-        System.out.printf(String.format("%n"));
-        System.out.println("Nom del client: " + nomCli);
-        System.out.println("Telefon del client: " + telCli);
-        System.out.println("Data de l'encarrec: " + dataEncarrec);
-        System.out.printf(String.format("%n"));
-        System.out.printf(String.format("%-15s %-10s %-15s%n", "Quantitat","Unitats","Article"));
-        System.out.printf(String.valueOf("=").repeat(15)+" " +String.valueOf("=").repeat(10)+" "+String.valueOf("=").repeat(15));
-        for (Article article:articles) {
-            System.out.printf(String.format("%n"));
-            System.out.printf(String.format("%-15s %-10s %-15s%n",article.getnombreUnitats(),article.gettipusUnitat(),article.getNomArticle()));
-        }
-    }
-
-    public static void TextMultiLinea(String nomCli, String telCli, String dataEncarrec, ArrayList<Article> articles, String fileName) {
-    /* Aquest mètode s'encarrega d'agafar els detalls de l'encarrec i formatar-lo a un fitxer pla,
-     * el qual no és un csv sinó una mena de comprovant pel client
-     */
-
-        try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(fileName))) {
-            
-            bw1.write("Nom del client: " + nomCli);
-            bw1.newLine();
-            bw1.write("Telefon del client: " + telCli);
-            bw1.newLine();
-            bw1.write("Data de l'encarrec: " + dataEncarrec);
-            bw1.newLine();
-            bw1.write(String.format("%-15s %-10s %-15s%n", "Quantitat","Unitats","Article"));
-            bw1.write(String.valueOf("=").repeat(15)+" " +String.valueOf("=").repeat(10)+" "+String.valueOf("=").repeat(15));
-            bw1.newLine();
-            for (Article article:articles) {
-                bw1.write(String.format("%-15s %-10s %-15s%n",article.getnombreUnitats(),article.gettipusUnitat(),article.getNomArticle()));
-                bw1.newLine();
-            }
-
-        } catch (Exception e) {
-            System.out.println("Error");
-        } 
-
-    }
-
-    public static void csvLinea(String nomCli, String telCli, String dataEncarrec, ArrayList<Article> articles, String fileName) {
-    /* Aquest mètode s'encarrega d'agafar els detalls de l'encarrec i formatar-lo a un fitxer pla separat per comes (csv)
-     */
-        try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(fileName))){
-
-            String csvArticles = "";
-
-            String csvLinea = nomCli + ";" + telCli + ";" + dataEncarrec + ";";
-
-            for (Article article : articles) {
-                csvArticles =  csvArticles + 
-                            article.getNomArticle() + ";" + 
-                            article.getnombreUnitats() + ";" +
-                            article.gettipusUnitat() + ";" ;
-            }
-
-            bw1.write(csvLinea + csvArticles);
-
-            
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-
-    }
-
-    public static void csvLineaObjEn(Encarrec encarrec, String fileName) {
-    /* Aquest mètode és una alternativa per si es fa servir una classe anomenada Encarrec, la qual 
-     * fa servir la classe Article
-     */
-        try (BufferedWriter bw1 = new BufferedWriter(new FileWriter(fileName))){
-
-            bw1.write(encarrec.toCSV());
-            
-        } catch (Exception e) {
-            System.out.println("Error");
-        }
-    }
-
-
-    public static void binari (String nomCli, String telCli, String dataEncarrec, ArrayList<Article> articles, String fileName) {
-    /* Aquest mètode s'encarrega d'agafar els detalls de l'encarrec i formatar-lo a un fitxer binari
-     */
-
-        try (DataOutputStream ds1 = new DataOutputStream(new FileOutputStream(fileName))) {
-
-            ds1.writeUTF(nomCli);
-            ds1.writeUTF(telCli);
-            ds1.writeUTF(dataEncarrec);
-            
-            for (Article article:articles) {
-                ds1.writeUTF(article.getNomArticle());
-                ds1.writeFloat(article.getnombreUnitats());
-                ds1.writeUTF(article.gettipusUnitat());
-            }
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
+            ur1.FormatBinari(folder, fileName);
         }
 
     }
--- README.md
+++ README.md
@@ -1,1 +1,54 @@
-# M06-UF1-A01
+# **Funcionament de l'aplicació**
+
+L'aplicació té dues funcionalitats principals:
+
+* Agafar per pantalla les dades d'un encàrrec i generar un fitxer.
+* Mostrar les dades d'un encàrrec a partir del que es llegeix d'un fitxer.
+
+## Estructura del projecte
+
+El projecte s'ha organitzat entorn dos paquets: un anomenat App i un altre anomenat Model.
+
+Model: conté les classes que formen part del model de l'aplicació.
+App: conté les classes que tenen la lògica de l'aplicació interaccionant amb les classes del model.
+
+## El Main de l'Aplicació
+
+El mètode MainMenu() mostra les tres opcions disponibles: escriure, llegir i sortir.
+
+El mètode DemanarOpcio() dins Aplicacio és qui orquestra totes les accions, en aquest ordre:
+
+* Obrir un bufferedReader per tal de demanar les dades per consola.
+* Segons l'opció triada:
+    * Si és 1, ens condueix a AfegirDadesEncarrec().
+    * Si és 2, ens condueix a MostrarEncarrec().
+    * Si és 3, sortim de l'aplicació.
+    * Qualsevol altra opció introduïda ens conduirà un altre cop a demanar que s'introdueixi 1, 2 o 3.
+
+## Afegir dades d'encàrrec: generació de fitxers
+
+Amb el buffer obert es demanen les dades per generar l'encàrrec: nom del client, telèfon, data de l'encàrrec i els articles de l'encàrrec. Es podran introduir diferents articles fins que es desitgi.
+
+A partir d'aquí fem ús de la classe UtilWriteFitxer on estan continguts els mètodes d'escriptura en diferents formats: text multilínia, csv o binari.
+
+L'usuari haurà de triar en quin format vol escriure el fitxer.
+
+ALTERNATIVES:
+* Generar tots tres fitxers sense demanar a l'usuari que esculli un.
+* Un cop s'ha generat un format demanar a l'usuari si vol generar el mateix encàrrec amb un altre format.
+
+## Mostrar dades d'encàrrec: lectura de fitxers
+
+Amb el buffer encara obert ens demanaran saber el format del fitxer que volem llegir i el seu nom.
+
+L'aplicació assumeix que el fitxer a tractar es troba en el mateix directori on prèviament aquesta aplicació els ha desat (en el cas de l'exemple és C:\\Users\\accesadades\\). Es pot perfectament canviar i fer servir qualsevol altre directori esmenant la variable folder que hi ha dintre Aplicació.
+
+De forma semblant a l'escriptura disposem de UtilReadFitxer on trobem els mètodes de lectura en dos formats: csv o binari.
+
+## Què no fa l'aplicació (i podria fer)?
+
+* Validar el format de dada que l'usuari introdueix per pantalla:
+    * Format de dates.
+    * Telèfons.
+
+* Actualment i per simplicitat (com ja es va dir a l'enunciat) cada encàrrec genera un fitxer diferent. Però l'aplicació podria agafar diferents encàrrecs en un mateix fitxer. O afegir-los a un fitxer existent.
